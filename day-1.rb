fb70a0c4bcfe0bf8be4e1c538428aa1217220c09
ricksterhd123 <ricksterhd123@gmail.com>
2023-12-21 01:58:49 +0000

    solve day-1 part 2

--- day-1.rb
+++ day-1.rb
@@ -5,8 +5,20 @@ input = file.read
 file.close
 
 lines = input.split(/\n/, -1).map do |line|
-  decoded_line = line.split('').filter { |x| /\d/.match?(x) }.reverse
-  pair = [decoded_line[0], decoded_line[decoded_line.length - 1]]
+  line_regex = /(?=(three|seven|nine|eight|four|five|six|two|one))|(\d)/
+  number_map = {
+    'one' => 1,
+    'two' => 2,
+    'three' => 3,
+    'four' => 4,
+    'five' => 5,
+    'six' => 6,
+    'seven' => 7,
+    'eight' => 8,
+    'nine' => 9
+  }
+  decoded_line = line.scan(line_regex).flatten.compact.filter { |x| line_regex.match?(x) }.map { |x| number_map[x] || x.to_i }
+  pair = [decoded_line[0], decoded_line[decoded_line.length - 1]].reverse
   pair.map.with_index { |x, i| x.to_i * (10 ** i) }.reduce(:+)
 end
 
