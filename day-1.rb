df8cec8d286985f206a79121ebb20d61357e1e73
ricksterhd123 <ricksterhd123@gmail.com>
2023-12-21 02:01:27 +0000

    minify

--- day-1.rb
+++ day-1.rb
@@ -4,22 +4,12 @@ file = File.open(INPUT_FILE)
 input = file.read
 file.close
 
-lines = input.split(/\n/, -1).map do |line|
-  line_regex = /(?=(three|seven|nine|eight|four|five|six|two|one))|(\d)/
-  number_map = {
-    'one' => 1,
-    'two' => 2,
-    'three' => 3,
-    'four' => 4,
-    'five' => 5,
-    'six' => 6,
-    'seven' => 7,
-    'eight' => 8,
-    'nine' => 9
-  }
-  decoded_line = line.scan(line_regex).flatten.compact.map { |x| number_map[x] || x.to_i }
-  pair = [decoded_line[0], decoded_line[decoded_line.length - 1]].reverse
-  pair.map.with_index { |x, i| x.to_i * (10 ** i) }.reduce(:+)
-end
+line_regex = /(?=(three|seven|nine|eight|four|five|six|two|one))|(\d)/
+number_map = {
+  'one' => 1, 'two' => 2, 'three' => 3, 'four' => 4, 'five' => 5, 'six' => 6, 'seven' => 7, 'eight' => 8, 'nine' => 9
+}
 
-puts lines.reduce(:+)
+puts input.split(/\n/, -1).map { |line|
+  decoded_line = line.scan(line_regex).flatten.compact.map { |x| number_map[x] || x.to_i }
+  [decoded_line[0], decoded_line[decoded_line.length - 1]].reverse.map.with_index { |x, i| x.to_i * (10 ** i) }.reduce(:+)
+}.reduce(:+)
