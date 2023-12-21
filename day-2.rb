819b32624c93b28e7f05b84f31f853ba98ada32b
ricksterhd123 <ricksterhd123@gmail.com>
2023-12-21 03:40:18 +0000

    refactor some more

--- day-2.rb
+++ day-2.rb
@@ -6,15 +6,14 @@ file.close
 
 lines = input.split(/\n/).map do |line|
   game_meta, game = line.split(/: /)
-  game_meta_match = game_meta.match(/Game (\d+)/)
-  game_number = game_meta_match[1].to_i
+  _, game_number = game_meta.split(/ /)
 
-  game_sets = game.split(/;/).map { |set| set.strip.split(/, /).map do |draw|
-    match = /(\d+) (red|green|blue)/.match(draw)
-    [match[1].to_i, match[2]]
-  end }
-
-  flattened_game_sets = game_sets.reduce([], :concat)
+  flattened_game_sets = game.split(/;/).map { |set|
+    set.strip.split(/, /).map { |draw|
+      match = /(\d+) (red|green|blue)/.match(draw)
+      [match[1].to_i, match[2]]
+    }
+  }.reduce([], :concat)
 
   red, green, blue = [0, 0, 0]
   for n, color in flattened_game_sets do
@@ -27,12 +26,10 @@ lines = input.split(/\n/).map do |line|
     end
   end
 
-  total = red + green + blue
-  [red, green, blue, total, game_number]
+  [red, green, blue, red + green + blue, game_number.to_i]
 end
 
 puts lines
-.sort { |game1, game2| game2[3] <=> game1[3] }
-.filter { |game| game => [red, green, blue, _, _]; red <= 12 and green <= 13 and blue <= 14 }
-.map { |game| game[4] }
+.sort { |game1, game2| game2[3] <=> game1[3] }  # total the maxes and sort descending
+.filter_map { |game| game => [red, green, blue, _, _]; game[4] if red <= 12 and green <= 13 and blue <= 14 }
 .reduce(:+)
