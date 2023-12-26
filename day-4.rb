9527518d067f5d8066410c132604a9dbdc13bee0
ricksterhd123 <ricksterhd123@gmail.com>
2023-12-26 15:18:55 +0000

    solution to day 4 part 2

--- day-4.rb
+++ day-4.rb
@@ -18,10 +18,46 @@ def lex_cards(input)
   end
 end
 
-def scratch_card(card)
+def get_card_winning_numbers(card)
   card => [_, winning_numbers, card_numbers]
-  winning_card_numbers = (winning_numbers & card_numbers)
+  (winning_numbers & card_numbers)
+end
+
+def get_card_points1(card)
+  winning_card_numbers = get_card_winning_numbers(card)
   winning_card_numbers.length > 0 ? 2 ** (winning_card_numbers.length - 1) : 0
 end
 
-puts JSON.dump(lex_cards(input).map{ |card| scratch_card(card) }.reduce(:+))
+cards = lex_cards(input)
+
+card_score_map = {}
+card_map = {}
+
+for card in cards do
+  card => [card_index, _, _]
+  winning_card_numbers = get_card_winning_numbers(card)
+  card_map[card_index] = winning_card_numbers.map.with_index { |_, i| card_index + i + 1 }
+  card_score_map[card_index] = winning_card_numbers.length
+end
+
+def get_card_copies(cache, card_map, card_index)
+  if cache[card_index] then
+    return cache[card_index]
+  end
+
+  card_copies = card_map[card_index]
+  if card_copies.length > 0
+    cache[card_index] = [card_index, card_copies.map { |copy_card_index| get_card_copies(cache, card_map, copy_card_index) }.flatten]
+  else
+    cache[card_index] = [card_index]
+  end
+end
+
+cache = {}
+
+puts JSON.dump(cards.map{ |card| get_card_points1(card) }.reduce(:+))
+puts JSON.dump(
+  cards
+  .sort { |card_a, card_b| card_score_map[card_a[0]] <=> card_score_map[card_b[0]]}
+  .reduce(0) { |acc, card| acc + get_card_copies(cache, card_map, card[0]).flatten.length }
+)
